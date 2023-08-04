196c6fbb7137ed643aa22d557b138e6132d3df2e
diegdev <diegveloper@gmail.com>
2023-08-04 18:42:24 -0400

    test

--- inc/plugins/mini-cart-qty.php
+++ inc/plugins/mini-cart-qty.php
@@ -91,7 +91,7 @@ add_action('wp_footer', function(){
         }
         .widget_shopping_cart_content .mini-cart-qty-wrapper .quantity input[type=number]{
             min-height: 26px;
-            color: #444;
+            color: #000;
             background: white;
             font-size: 16px;
             border: none !important;
